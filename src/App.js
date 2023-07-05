aab5cf764f1f20b34f50c462990d38f1811d4864
priteshsarva <priteshsarva9825@gmail.com>
2023-07-06 00:49:20 +0530

    test for orientatiom

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 import ParallaxPage from './components/ParallaxPage';
 
 const App = () => {
+  let xValue = '16'
+  let yValue
+
+  window.addEventListener('load', (e) => {
+
+    // console.log(document.getElementById('xvalue').innerHTML = "yooo");
+    window.addEventListener("deviceorientation", (e) => {
+      xValue = e.beta
+      // xValue = "58"
+      // console.log(e);
+      console.log("hello");
+      document.getElementById('xvalue').innerHTML = `${xValue} px`
+
+    })
+
+  })
+
   return (
     <>
-      <ParallaxPage />
+      <h1 id='xvalue'>{xValue} px</h1>
+      <h1 id='yvalue'>Y px</h1>
+
+      {/* <ParallaxPage /> */}
     </>
   )
 }
