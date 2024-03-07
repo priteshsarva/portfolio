8fe561783cb357cc4be4da86c3ffcd4d762e06e8
priteshsarva <priteshsarva9825@gmail.com>
2024-03-07 09:56:28 +0530

    7-3-24

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,14 +2,25 @@ import React from 'react'
 import './Contact.css'
 import LampScreen from "../../assets/images/Lamp/LampScreen.png";
 import LampBg from "../../assets/images/Lamp/lampBG.png";
-import LampMultiply from "../../assets/images/Lamp/LampMultiply.png";
 
 const Contact = () => {
+
+    //     LampScreen
+    // LampMultiply
     return <>
+
         <div className="lamp">
-            <img src={LampBg} alt="" />
-            <img src={LampScreen} alt="" />
-            <img src={LampMultiply} alt="" />
+            <input type="checkbox" id="lamp-toggle" class="visually-hidden" />
+            <h1 className='contact-heading'>Get in Touch</h1>
+            <a href="mailto:priteshsarva9825@gmail.com" className='contact-link'>priteshsarva9825@gmail.com</a>
+            <br />
+            <a href="tel:+919408386083" className='contact-link bottom-link'>+91 94083 86083</a>
+            <img src={LampBg} alt="" className='LampBg' />
+            <img src={LampScreen} alt="" className='LampScreen' />
+            <label htmlFor="lamp-toggle" className='Lamplable'>
+                <img src={LampScreen} alt="" className='LampMultiply' />
+            </label>
+           
         </div>
     </>
 }
